0b72a268062a2c2390790f2a27021ff659c162e2
BAEJUNSOO <qornwh@gmail.com>
2025-10-02 17:57:28 +0900

    ssafy first commit

---
+++ work/common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#define COMMON
+#define PIPE_NAME "~/ssafy/ssafy/work/pipe"
+#endif
